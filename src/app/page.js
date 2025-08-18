f6c26c8b623120d43577a34a7deba1b83382e949
aryankumarsingh <aryankumarsingh8340@gmail.com>
2025-08-18 21:41:56 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-br from-blue-50 to-blue-100 flex flex-col">
+      
+  );
+}
